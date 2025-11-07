e2433a73653953557d79714e1ec9d8f9dbb90837
yu.takada <yu.takada@monocrea.co.jp>
2025-11-07 19:33:39 +0900

    update

---
+++ demo/demo.test.ts
@@ -0,0 +1,11 @@
+import { add, multiply } from "./demo";
+
+test('add function', () => {
+  expect(add(2, 3)).toBe(5);
+  expect(add(-1, 1)).toBe(0);
+});
+
+test('multiply function', () => {
+  expect(multiply(3, 4)).toBe(12);
+  expect(multiply(0, 5)).toBe(0);
+});
